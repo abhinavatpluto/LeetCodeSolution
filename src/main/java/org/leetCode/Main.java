3dabe923f2725a287b0b5e4799efc5b1f7f658bd
Abhinav Kumar <abhinavatpluto@gmail.com>
2025-04-07 20:58:12 +0530

    new code

--- src/main/java/org/leetCode/Main.java
+++ src/main/java/org/leetCode/Main.java
@@ -53,32 +53,17 @@ public class Main {
         }
     }
 
-//    static int convertfive(int num) {
-//      Char [] arr = String.valueOf(num).toCharArray();
-//
-//        for(int i = 0; i < arr.length; i++){
-//            if(arr[i] == '0'){
-//                arr[i] = '5';
-//            }
-//        }
-//      return 0;
-//    }
-
     public int[] minAnd2ndMin(int arr[]) {
-       int min = Integer.MAX_VALUE;
-         int secondMin = Integer.MAX_VALUE;
-       for(int i = 0 ; i < arr.length; i++){
-           if(arr[i] < min){
-               min = arr[i];
-           }
-       }
-       for(int i = 0;  i < arr.length; i++){
-           if(arr[i] < secondMin && arr[i] != min){
-               secondMin = arr[i];
 
-           }
-       }
-        return new int[]{min, secondMin};
+        int n = arr.length;
+        if(n<2) return new int[]{-1};
+        Arrays.sort(arr);
+        for(int i = 1;i<n;i++){
+            if(arr[i]!=arr[0]){
+                return new int[]{arr[0],arr[i]};
+            }
+        }
+        return new int[]{-1};
     }
 
     public List<Integer> valueEqualToIndex(List<Integer> nums) {
@@ -92,14 +77,43 @@ public class Main {
         return result ;
     }
 
+    int convertfive(int num) {
+        String numStr = String.valueOf(num);
+        numStr = numStr.replace("0","5");
+
+           return Integer.parseInt(numStr);
+    }
+
+    public void countFreq(int[] arr){
+        HashMap<Integer,Integer> count = new HashMap<>();
+        for(int i : arr){
+            count.put(i,count.getOrDefault(i,0));
+        }
+    }
+
+    public void swapKth(List<Integer> arr, int k) {
+        int n = arr.size();
+        if (k > n) {
+            throw new IllegalArgumentException("k is larger than the size of the list");
+        }
+        int temp = arr.get(k - 1);
+        arr.set(k - 1, arr.get(n - k));
+        arr.set(n - k, temp);
+    }
+
     public static void main(String[] args) {
         int[] a = {9, 1, 30, 40, 5, 6, 7};
-        for (int i = 0; i< a.length; i++){
-//            System.out.print(a[i] + " ");
-            if(i == a[i]){
-                System.out.print(a[i] + " ");
-            }
-        }
+        int k = 2;
+        int lastK = a.length - k;
+        int firstK = k - 1;
+
+       int temp = a[lastK];
+       a[lastK] = a[firstK];
+        a[firstK] = temp;
+        System.out.println(Arrays.toString(a)); // Output: "15400"
+
+
+
 
     }
 
